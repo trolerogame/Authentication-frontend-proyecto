938e7b0fd8021cdca1c74b289a147fc1341a667d
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-27 22:57:41 -0300

    change redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useRouter } from 'next/router'
 import React,{useEffect} from 'react'
-import {getTokenCookie} from '../utils/getTokenCookie'
 const index = () => {
   const router = useRouter()
   useEffect(() => {
-    if(!getTokenCookie()) router.push('/login')
+    router.push('/login')
   },[router])
   return (
     <div></div>

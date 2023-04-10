61d4a6557025dc79df3652b62231728299e5b598
trolerogame <danielgcampos.04@gmail.com>
2023-04-09 21:00:12 -0300

    add constants url apis

---
+++ utils/urlApis.ts
@@ -0,0 +1,10 @@
+// DEPLOY
+// export const urlGraphql = 'https://authentication-node-server.herokuapp.com'
+// export const urlSocket = 'https://authentication-socket.herokuapp.com'
+
+
+// DEV
+
+export const urlGraphql = 'http://localhost:4040'
+export const urlSocket = 'http://localhost:4000'
+
